17d04610be96b08cddacdecafb8478311e3c1921
rjreddy111 <rjreddy11@gmail.com>
2024-08-29 17:37:44 +0530

    code before deployment

---
+++ adaptnxtBackend/Readme.md
@@ -0,0 +1,18 @@
+###
+Commands used to setup the project and packages
+1. mkdir adaptnxtBackend (to create app-name)
+2. cd /adaptnxtBackend (to navigate into the app directory)
+3. npm init -y  (to set up the project)
+4. npm install express (to use express framework)
+5. node app.js  to run the project
+
+###
+API used : https://api.weatherstack.com/current?access_key=${access_token}&query=${city} 
+
+* access_token = API KEY 
+* city = "city name" to fetch details based on query parameter
+
+
+
+
+
---
+++ adaptnxtBackend/app.js
@@ -0,0 +1,122 @@
+const express = require("express")
+
+
+
+const app = express()
+
+
+const access_token = "6c7a3da2449cbe2b5682d1ab2047132e"
+
+app.get("/weather", async(request,response)=> {
+    
+    const cityObject = request.query
+    console.log(cityObject)
+
+   let  city = request.query.query 
+         city  = encodeURIComponent(city)
+   console.log(city)
+
+   if (!city) {
+    return response.status(400).send({error:"City is required"})
+   }
+
+   
+
+
+   
+
+  try {
+
+    const url = `https://api.weatherstack.com/current?access_key=${access_token}&query=${city}`
+    console.log(url)
+    const getResponse = await fetch(url)
+    const data = await getResponse.json()
+    console.log(data)
+
+      //* we can destrcuture  object and use  
+      
+      const {location: {country,lat,lon,region,localtime,timezone_id,utc_offset}, 
+        current : {temperature,wind_speed,wind_dir,pressure,humidity,cloudcover,feelslike,visibility,is_day,weather_icons,weather_descriptions}
+    } = data 
+     response.json({
+        location: {
+            country, 
+            latitude:lat,
+            longitude:lon, 
+            region,
+            localTime: localtime,
+            timeZoneId:timezone_id,
+            utcOffset:utc_offset
+        },
+        current: {
+            temperature, 
+            windSpeed:wind_speed,
+            windDirection:wind_dir,
+            pressure,humidity,
+            cloudCover:cloudcover,
+            feelsLike:feelslike,
+            visibility,
+            isDay:is_day,
+            weatherIcons:weather_icons, 
+            weatherDescriptions:weather_descriptions
+
+        }
+     });
+
+    
+
+    
+
+/*
+    response.json({
+        location: {
+      
+            country : data.location.country, 
+            latitude : data.location.lat, 
+            longitude: data.location.lon,
+            region : data.location.region,
+            localTime:data.location.localtime, 
+            timeZoneId : data.location.timezone_id,
+            utcOffset : data.location.utc_offset
+
+
+        },
+         current : {
+
+            temperature : data.current.temperature,
+            windSpeed : data.current.wind_speed,
+            windDirection: data.current.wind_dir,
+            pressure:data.current.pressure,
+            humidity: data.current.humidity, 
+            cloudCover:data.current.cloudcover, 
+            feelsLike : data.current.feelslike, 
+            visibility: data.current.visibility, 
+            isDay : data.current.is_day,
+            weatherIcons : data.current.weather_icons,
+            weatherDescriptions: data.current.weather_descriptions
+
+
+
+         }
+        
+
+
+    }) */
+} 
+catch(error) {
+    response.status(500).send({error:"Failed to fetched weather Data, Please check the name of city"})
+}
+
+})
+
+
+
+app.listen(5001, ()=> {
+    console.log("server is ruunning on 5001")
+} )
+
+
+
+
+
+module.exports = app
